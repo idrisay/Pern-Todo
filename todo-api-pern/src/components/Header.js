9bca8731b15a30a6efda4f72024393c0aea9217b
Idris AY <97283155+idrisay@users.noreply.github.com>
2023-06-07 12:33:14 +0200

    feat: add icons

--- todo-api-pern/src/components/Header.js
+++ todo-api-pern/src/components/Header.js
@@ -1,16 +1,16 @@
 import React from "react";
-import cw from "../assets/cw.svg";
+import hiCoders from "../assets/hiCoders.png";
 import "./style.css";
 
 const Header = () => {
   return (
     <div>
       <div className="text-center">
-        <img src={cw} alt="clarusway" className="cw" />
+        <img src={hiCoders} alt="clarusway" className="cw" />
         <h6 className="text-center mt-5">
-          This app has been developed by Clarusway Developers.
+          This app has been developed by HiCoders Developers.
         </h6>
-        <h1 className="text-center mt-5 header-text">Clarus Todos</h1>
+        <h1 className="text-center mt-5 header-text">HiCoders Todos</h1>
       </div>
     </div>
   );
--- todo-app-pern/db.js
+++ todo-app-pern/db.js
@@ -1,18 +1,3 @@
-// const Pool = require("pg").Pool;
-
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   database: process.env.DB_NAME,
-// });
-
-// module.exports = pool;
-
-// const databaseConfig = { connectionString: process.env.DATABASE_URL };
-// const pool = new Pool(databaseConfig);
-// module.exports = pool;
 
 const { Client } = require("pg");
 
